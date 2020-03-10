26a37a71106b8edd7f44015eebb2b2ca0ca89102
shibly <shibly@sileria.com>
2020-03-09 17:06:32 -0700

    Implement get/store features in db and pull features from remote

--- app/src/main/java/com/example/hsexercise/common/Repository.kt
+++ app/src/main/java/com/example/hsexercise/common/Repository.kt
@@ -1,5 +1,12 @@
 package com.example.hsexercise.common
 
+import android.annotation.SuppressLint
+import android.util.Log
+import com.example.hsexercise.feature.database.FeatureModel
+import io.reactivex.Flowable
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
+
 /**
  * Repository - Facade layer for all persisted data retrieval.
  */
@@ -10,4 +17,35 @@ object Repository {
 
     private val TAG = Repository::class.java.simpleName
 
+    /** Get Features from database */
+    private fun getFeaturesFromDb (): Flowable<List<FeatureModel>> {
+        return database.featureTableDao().getAll()
+            .toFlowable()
+            .doOnNext {
+                Log.d(TAG,"Dispatching ${it.size} features from DB...")
+            }
+    }
+
+    /** Get Features from api and cache them to database */
+    private fun getFeaturesFromApi(): Flowable<List<FeatureModel>> {
+        return api.getFeatures()
+            .onErrorReturn { e -> Log.d(TAG, "Request failed $e"); emptyList() }
+            .doOnNext {
+                Log.d(TAG,"Dispatching ${it.size} features from API...")
+                storeFeaturesInDb(it)
+            }
+    }
+
+    fun getFeatures() : Flowable<List<FeatureModel>> = getFeaturesFromApi()
+
+    @SuppressLint("CheckResult")
+    /** Store features in database */
+    private fun storeFeaturesInDb(users: List<FeatureModel>) {
+        Observable.fromCallable { database.featureTableDao().insertAll(users) }
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.io())
+            .subscribe {
+                Log.d(TAG, "Inserted ${users.size} features from API in DB...")
+            }
+    }
 }
--- app/src/main/java/com/example/hsexercise/feature/FeatureViewModel.kt
+++ app/src/main/java/com/example/hsexercise/feature/FeatureViewModel.kt
@@ -2,6 +2,7 @@ package com.example.hsexercise.feature
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.hsexercise.common.Repository
 
 class FeatureViewModel : ViewModel() {
 
@@ -9,5 +10,9 @@ class FeatureViewModel : ViewModel() {
         ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel> create(modelClass: Class<T>) = FeatureViewModel() as T
+    }
+
+    fun getFeatures() {
+        Repository.getFeatures()
     }
 }
